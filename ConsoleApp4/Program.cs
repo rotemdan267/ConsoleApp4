b262098dcb1dd87678d4aeb53ac74547de9a6a9f
rotemdan267 <rotemdan267@gmail.com>
2021-07-22 04:32:46 +0300

    Add project files.

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person p = new Student();
+            p.ChangeAge();
+            
+
+            Console.ReadLine();
+        }
+    }
+    class Person
+    {
+        public string name;
+        public int age;
+
+        public virtual void ChangeAge()
+        {
+            age++;
+        }
+    }
+    class Student : Person
+    {
+        public int grade;
+        public override void ChangeAge()
+        {
+            age--;
+            grade++;
+        }
+        public void ChangeGrade()
+        {
+            grade += 10;
+        }
+    }
+}
